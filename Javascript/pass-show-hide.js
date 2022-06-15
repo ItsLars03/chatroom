d1a65cc8bbe5412a89c7079dfecc23f5dc3df82d
Martiess <232710ROC@gmail.com>
2022-06-15 09:41:33 +0200

    Push Database

--- Javascript/pass-show-hide.js
+++ Javascript/pass-show-hide.js
@@ -4,5 +4,10 @@ toggleBtn = document.querySelector(".form .field i");
 toggleBtn.onclick = ()=>{
     if(pswrdField.type == "password") {
         pswrdField.type = "text";
-    }
+        toggleBtn.classList.add("active");
+    
+}else{
+    pswrdField.type= "password"
+    toggleBtn.classList.remove("active");
+}
 }
