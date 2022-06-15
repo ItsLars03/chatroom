35aa62c390743bf1cf052abc1493966046b7cbed
Martiess <232710ROC@gmail.com>
2022-06-15 11:29:27 +0200

    PHP Data Inserrt

--- Javascript/sign.js
+++ Javascript/sign.js
@@ -1,14 +1,16 @@
-const form = document.querySelector(".signup form"),
-continueBtn = form.querySelector(".button input");
+const form = document.querySelector(".signup form")
+let continueBtn = document.getElementById("continue-to-chat-button");
+
 
 form.onsumbit = (e)=>{
     e.preventDefault(); 
 }
 
-continueBtn.onClick = ()=>{
+continueBtn.addEventListener("click", ()=>{
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/signup.php", true);
     xhr.onload = ()=>{
+        console.log(xhr)
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 let data = xhr.response;
@@ -16,7 +18,7 @@ continueBtn.onClick = ()=>{
             }
         }
     }
-
-    xhr.send();
-}
+    let formData = new FormData(form);
+    xhr.send(formData);
+} ) 
 
--- php/signup.php
+++ php/signup.php
@@ -1,5 +1,53 @@
 <?php
 
-echo "Hello";
+include_once "config.php";
+$fname = mysqli_real_escape_string($conn,$_POST['fname']);
+$lname = mysqli_real_escape_string($conn,$_POST['lname']);
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+$password = mysqli_real_escape_string($conn,$_POST['password']);
 
+if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+        $sql = mysqli_query($conn, "SELECT email FROM users WHERE email = '{$email}'");
+        if(mysqli_num_rows($sql) > 0){
+            echo "$email - This email already exist";
+        }else{
+            if(isset($_FILES['image'])){
+                $img_name = $_FILES['image']['name'];
+                $tmp_name = $_FILES['image']['tmp_name'];
+
+                $img_explode = explode('.', $img_name);
+                $img_ext = end($img_explode);
+
+                $extenstions = ['png', 'jpeg', 'jpg'];
+                if(in_array($img_ext, $extenstions) === true){
+                    $time = time();
+                    $new_img_name = $time.$img_name;
+                 if(   move_uploaded_file($tmp_name, "userimages/".$time.$img_name)){
+                    $status = "Active now";
+                    $random_id = ran(time(), 1000000);
+
+                    $sql2 = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status  
+                                        VALUES  ({$random_id}, '{$fname}', '{$lname}','{$email}','{$password}','{$new_img_name}',{$status})");
+                 }
+                }else {
+                    echo "Please select an Image file - jpeg, jpg, png!";
+                }
+
+
+
+            }else{
+                echo "Please select an Image file!";
+            }
+        }
+        
+    }else{
+        echo "$email - This is not a valid email!";
+    }
+
+}else {
+    echo "All input fields are required!";
+}
 ?>
