fe5adb761d35b2dbdc427dc1fcd9f07f3c2ce721
Martiess <fransdeboer4@gmail.com>
2022-06-21 14:24:00 +0200

    Push

--- chat.php
+++ chat.php
@@ -6,28 +6,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="styles-light.css">
+    <link rel="stylesheet" href="Styles/styles-dark.css">
     <title>Chattry</title>
 </head>
 <body>
     <div class="wrapper">
-        <section class="users">
-
+        <section class="chat-area">
         <header>
-            <div class="content">
-            <img src="/image/profile.png" alt="">
-            <div class="details"></div>
+            <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+            <img src="profile.png" alt="">
+            <div class="details">
                 <span>Gerbuiker</span>
-                <p>Active now</p>
-            </div>
-            <a href="#" class="logout">Logout</a>
+                <p>Active</p>
+         </div>  
         </header>
-        <div class="search">
-            <span class="text">Select a user to start a conversation with</span>   
-             <input type="text" placeholder="Enter name">
-             <button><i class="fas fa-search"></i></button>
+        <div class="chat-box">
+            <div class="chat outgoing">
+                <div class="details">
+                 <p>dit is een testbericht</p>   
+                </div>
+            </div>
+            <div class="chat incoming">
+                <img src="profile.png" alt="">
+                <div class="details">
+                 <p>dit is een test</p> 
+                </div>
+            </div>
+            <div class="chat outgoing">
+                <div class="details">
+                 <p>hallo</p>   
+                </div>
+            </div>
+            <div class="chat incoming">
+                <img src="profile.png" alt="">
+                <div class="details">
+                 <p>hallo</p> 
+                </div>
+            </div>
+            <div class="chat outgoing">
+                <div class="details">
+                 <p>hallo</p>   
+                </div>
+            </div>
+            <div class="chat incoming">
+                <img src="profile.png" alt="">
+                <div class="details">
+                 <p>hallo</p> 
+                </div>
+            </div>
+            <div class="chat outgoing">
+                <div class="details">
+                 <p>hallo</p>   
+                </div>
+            </div>
+            <div class="chat incoming">
+                <img src="profile.png" alt="">
+                <div class="details">
+                 <p>hallo</p> 
+                </div>
+            </div>
         </div>
-        </section>
-    </div>
+    <form action="#" class="typing-area">
+        <input type="text" placeholder="Type a message here...">
+        <button><i class="fab fa-telegram-plane"></i></button>
+    </form>
+    </section>
 </body>
 </html>
