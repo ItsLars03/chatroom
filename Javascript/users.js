fc8728f5b5f018eecd5738af13ba4d5b9d767fee
ItsLars03 <74597365+ItsLars03@users.noreply.github.com>
2022-06-22 09:24:26 +0200

    -

--- Javascript/users.js
+++ Javascript/users.js
@@ -1,5 +1,5 @@
 const searchBar = document.querySelector(".users .search input"),
-searchBtn = document.querySelector(".users .search button"),
+searchBtn = document.querySelector(".users .search button"), 
 usersList = document.querySelector(".users .users-list");
 
 searchBtn.onclick = ()=>{
@@ -8,9 +8,25 @@ searchBtn.onclick = ()=>{
     searchBtn.classList.toggle("active");
 }
 
+searchBar.onkeyup = ()=>{
+    let searchTerm = searchBar.ariaValueMax;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/search.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+              let data = xhr.response;
+              console.log(data);
+          }
+      }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhr.send("searchTerm" + searchTerm);
+}
+
 setInterval(()=>{
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/users.php", true);
+    xhr.open("GET", "php/users.php", true);
     xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
